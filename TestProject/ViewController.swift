ef43989be2a1f55f1083a0b27f2a53f939791b81
Saravanan R <saravananr@Saravanans-MacBook-Pro.local>
2017-09-04 14:05:22 +0530

    phase1

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
       print("kuya")
+        print("tech")
     }
 
     override func didReceiveMemoryWarning() {
